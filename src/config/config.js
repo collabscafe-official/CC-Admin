b0932de6b5f878d241b8f2b454f97e692ce70ff5
kbabdul <71380695+abdulrehmanrfqt@users.noreply.github.com>
2025-11-29 17:09:49 +0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     //  base_url: process.env.NODE_ENV==="production"?process.env.REACT_APP_BASE_URL:'http://172.16.1.157:9000',
     // base_url: process.env.REACT_APP_BASE_URL,
-    // base_url: 'https://cclive.collabscafe.com/v1',
-    base_url: 'https://devbe.collabscafe.com/v1',
+    base_url: 'https://cclive.collabscafe.com/v1',
+    // base_url: 'https://devbe.collabscafe.com/v1',
 };
 export default config; 

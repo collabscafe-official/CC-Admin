acf7944fccc4dc8ea3e6a1c97b4468b54f6bf7ff
mudassir <mud27747@gmail.com>
2026-04-03 13:30:38 +0500

    Config-changes

--- src/config/emailService.ts
+++ src/config/emailService.ts
@@ -1,8 +1,8 @@
 const EMAIL_CONFIG = {
-  //BASE_URL: 'https://emailapi.collabscafe.com',
-  //API_KEY: 'FHsbN6M6xc8g',
-  BASE_URL: 'http://localhost:4000',
-  API_KEY: 'cc-email-admin-2026',
+  BASE_URL: 'https://emailapi.collabscafe.com',
+  API_KEY: 'FHsbN6M6xc8g',
+  //BASE_URL: 'http://localhost:4000',
+  //API_KEY: 'cc-email-admin-2026',
 };
 
 export default EMAIL_CONFIG;
